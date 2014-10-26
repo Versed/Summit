30cea7ef110bbd0148e705b21923bd7edc5645db
Nathan Kluth <nlkluth@gmail.com>
2014-10-26 10:07:22 -0600

    creating users resource

--- summit/db/schema.rb
+++ summit/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141026030501) do
+ActiveRecord::Schema.define(version: 20141026044408) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -39,5 +39,17 @@ ActiveRecord::Schema.define(version: 20141026030501) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "group"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email"
 
 end

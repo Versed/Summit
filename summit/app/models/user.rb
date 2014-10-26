4b3545076ebef18470da4ca20e6dd9ce710c8729
Nathan Kluth <nlkluth@gmail.com>
2014-10-26 10:29:48 -0600

    validate email format

--- summit/app/models/user.rb
+++ summit/app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true,
+            format: {
+              with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/
+            }
   before_save :downcase_email
 
   def downcase_email
